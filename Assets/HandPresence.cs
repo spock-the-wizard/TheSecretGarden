63b817faf15a26d79b936f2468ffdcd39b83189f
RisingStockPrices <thswndms98@naver.com>
2020-11-22 23:13:06 +0900

    :sparkles: basic setup for controller animations

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
+    
+    private InputDevice targetDevice;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            Debug.Log(targetDevice.name);
+        }
+
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+
+    }
+
+    void UpdateAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+            handAnimator.SetFloat("Trigger", 0);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+            handAnimator.SetFloat("Grip", 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAnimation();
+    }
+}
